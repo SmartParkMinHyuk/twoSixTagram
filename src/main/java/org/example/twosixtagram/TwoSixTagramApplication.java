42d45c2b6b16010e983c9720532d5a354286826f
DESKTOP-M7S61C8\박수박 <pospara2@gmail.com>
2025-04-07 20:25:26 +0900

    chor : commit init

---
+++ src/main/java/org/example/twosixtagram/TwoSixTagramApplication.java
@@ -0,0 +1,13 @@
+package org.example.twosixtagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoSixTagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoSixTagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/twosixtagram/TwoSixTagramApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.twosixtagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoSixTagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
