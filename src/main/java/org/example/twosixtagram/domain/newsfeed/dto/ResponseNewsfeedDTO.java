872216993a90e00c3b344dbe7b34027d94214ffc
Prototype-Km <kgm970910@gmail.com>
2025-04-08 20:46:52 +0900

    feat(Newsfeed) : 작성, 삭제 (댓글전체리팩토링 필요) 완료

--- src/main/java/org/example/twosixtagram/domain/newsfeed/dto/ResponseNewsfeedDTO.java
+++ src/main/java/org/example/twosixtagram/domain/newsfeed/dto/ResponseNewsfeedDTO.java
@@ -1,4 +1,28 @@
 package org.example.twosixtagram.domain.newsfeed.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.example.twosixtagram.domain.newsfeed.entity.NewsFeed;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@Getter
 public class ResponseNewsfeedDTO {
+    private String contents;
+    private String title;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    // Entity -> DTO 변환
+    public static ResponseNewsfeedDTO toDTO(NewsFeed newsFeed) {
+        return ResponseNewsfeedDTO.builder()
+                .title(newsFeed.getTitle())
+                .contents(newsFeed.getContents())
+                .createdAt(newsFeed.getCreatedAt())
+                .build();
+    }
 }
--- src/main/java/org/example/twosixtagram/domain/newsfeed/entity/NewsFeed.java
+++ src/main/java/org/example/twosixtagram/domain/newsfeed/entity/NewsFeed.java
@@ -2,13 +2,16 @@ package org.example.twosixtagram.domain.newsfeed.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import org.example.twosixtagram.domain.comment.entity.Comment;
 import org.example.twosixtagram.domain.common.auditing.BaseEntity;
 import org.example.twosixtagram.domain.user.entity.User;
 
 @Entity
 @Table(name = "newsfeed")
 @Getter
+@Builder
 @AllArgsConstructor
 public class NewsFeed extends BaseEntity {
 
@@ -25,9 +28,13 @@ public class NewsFeed extends BaseEntity {
     @Column(nullable = false)
     private Long userid;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id",nullable = false)
+    private Comment comment;
 
     public NewsFeed() {
     }
