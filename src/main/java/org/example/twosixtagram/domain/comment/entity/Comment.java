0bf7397de943bb4d87a79bb934917c6ac1b713e0
supremhum <kalystols@gmail.com>
2025-04-08 10:37:46 +0900

    add: comment entity

--- src/main/java/org/example/twosixtagram/domain/comment/entity/Comment.java
+++ src/main/java/org/example/twosixtagram/domain/comment/entity/Comment.java
@@ -1,12 +1,37 @@
 package org.example.twosixtagram.domain.comment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.twosixtagram.domain.common.auditing.BaseEntity;
+import org.example.twosixtagram.domain.newsfeed.entity.NewsFeed;
+import org.example.twosixtagram.domain.user.entity.User;
 
+@NoArgsConstructor
 @Entity
-@Table(name="comment")
-public class Comment {
+@Getter
+@Table(name = "comment")
+public class Comment extends BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "contents")
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private NewsFeed newsFeed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Comment( User user, NewsFeed feed,String contents) {
+        this.user = user;
+        this.newsFeed = feed;
+        this.contents = contents;
+
+    }
 }
